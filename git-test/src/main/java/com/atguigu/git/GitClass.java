824fdc51319fa2ce69d56694c92d56b4bef387e5
Liyajie <270231835@qq.com>
2022-04-18 16:03:20 +0800

    test第一次提交

---
+++ git-test/src/main/java/com/atguigu/git/GitClass.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class GitClass {
+
+    public static void main(String[] args) {
+        System.out.println("11111");
+        System.out.println("11111");
+        System.out.println("11111");
+    }
+}
