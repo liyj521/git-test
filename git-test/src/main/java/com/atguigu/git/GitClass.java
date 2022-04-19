bffd2e7f2a049548dea8e2c00cc2fffea777d282
springcloud-config <60772797+liyj521@users.noreply.github.com>
2022-04-19 10:21:18 +0800

    pull

--- git-test/src/main/java/com/atguigu/git/GitClass.java
+++ git-test/src/main/java/com/atguigu/git/GitClass.java
@@ -11,5 +11,6 @@ public class GitClass {
         System.out.println("2222");
         System.out.println("2222");
         System.out.println("3333");
+        System.out.println("3333");
     }
 }
